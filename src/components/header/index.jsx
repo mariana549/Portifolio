1e68e31c5b7d465dfd65cdd37d7c4f0e8df38818
Mariana Antonia <marievaristo889@gmail.com>
2024-05-13 09:05:33 -0300

    atualizações

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -14,12 +14,12 @@ function Header() {
                <img src={logo} alt="logo" className="w-32"/>
                <nav>
                   <ul className="flex items-center gap-2 text-gray-50">
-                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center"> <img src={cursos} alt="icone curso" 
+                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center transition-[0.5s] ease-in-out hover:-translate-y-1 hover:scale-110"> <img src={cursos} alt="icone curso" 
 className="w-5"/> Cursos</li>
-                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center"> <img src={projetos} alt="icone projetos" className="w-5"/> Projetos</li>
-                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center"> <img src={user} alt="user" className="w-5"/> Sobre mim</li>
-                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center"> <img src={home} alt="logo home" className="w-5"/> Home</li>
-                     <li className="text-xl p-2 bg-pink-500 hover:shadow-inner hover:shadow-[#0000008c] rounded-lg flex gap-2 items-center"> <img src={contato} alt="contato" className=""/> Contato</li>
+                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center transition-[0.5s] ease-in-out hover:-translate-y-1 hover:scale-110"> <img src={projetos} alt="icone projetos" className="w-5"/> Projetos</li>
+                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center transition-[0.5s] ease-in-out hover:-translate-y-1 hover:scale-110"> <img src={user} alt="user" className="w-5"/> Sobre mim</li>
+                     <li className="text-xl p-2 hover:bg-pink-500 rounded-lg flex gap-2 items-center transition-[0.5s] ease-in-out hover:-translate-y-1 hover:scale-110"> <img src={home} alt="logo home" className="w-5"/> Home</li>
+                     <li className="text-xl p-2 bg-pink-500 hover:shadow-inner hover:shadow-[#0000008c] rounded-lg flex gap-2 items-center transition-[0.5s] ease-in-out"> <img src={contato} alt="contato" className=""/> Contato</li>
                   </ul>
                </nav>
             </div>
