ba455b881ff2aa0f401d56fa98bf3d1ae4a118d3
Mariana Antonia <marievaristo889@gmail.com>
2024-05-13 10:42:15 -0300

    atualização

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -14,7 +14,7 @@ function Main({ repos }) {
 
   return (
     <main className="">
-      <section className="bg-white  flex flex-col justify-center ">
+      <section className="bg-pink-100  flex flex-col justify-center ">
         <Container>
           <div className="flex gap-4 justify-between items-center">
             <div>
@@ -32,32 +32,32 @@ function Main({ repos }) {
           </div>
         </Container>
       </section>
-      <section className="bg-pink-300 min-h-[500px]">
+      <section className="bg-pink-400 min-h-[500px]">
          <Container>
-         <div className="flex flex-col items-center p-12 gap-4">
+         <div className="flex flex-col items-center py-12 gap-4">
             <h1 className="text-5xl shinning-2 div1 mb-4">Sobre mim</h1>
             <p className="text-lg text-pink-950 font-medium">Eu estudo programação desde o ano passado, estudei por um tempo no inicio do ano mas tive que parar. quando foi em outubro eu iniciei no curso da devquest e desde lá estudos constantemente todos os dias.</p>
             <p className="text-lg text-pink-950 font-medium">Depois que entrei para devquest fui influenciada a mecher também com meu linkedin, que eu já possuia desde 2022, mas, eu nunca tinha postado nada. Fiquei meio aprenciva pois não sabia o que poderia postar, então comecei seguindo um monte de programadores e nisso fui tendo ideias. começei a posta em novembro lá algumas coisas e de lá para ca eu conquistei meu 7k de seguidores.</p>
             <p className="text-lg text-pink-950 font-medium">Meu processo até aqui vem sido interresante pois cada vez mais me interesso pela area e me vejo trabalhando nela futuramente. Eu realmente tenho uma facilidade com programação e quero cada vez mais me aprofundar na area.</p>
          </div>
-         <div className="flex flex-col items-center p-12 gap-4">
+         <div className="flex flex-col items-center py-12 gap-4">
             <h2 className="text-4xl shinning-2 div1 mb-4">Tecnologias</h2>
             <ul className="flex flex-wrap gap-2 justify-center">
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={js} alt="logo" className="w-40 "/> JavaScript</li>
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={html} alt="logo" className="w-40"/> HTML 5</li>
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={css} alt="logo" className="w-40"/> CSS 3</li>
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={react} alt="logo" className="w-40"/> React</li>
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={figma} alt="logo" className="w-40"/> Figma</li>
-              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded text-center"><img src={git} alt="logo" className="w-40"/> Git</li>
-            </ul>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={js} alt="logo" className="w-36 rounded-2xl"/></li>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={html} alt="logo" className="w-40"/></li>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={css} alt="logo" className="w-40"/></li>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={react} alt="logo" className="w-40"/></li>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={figma} alt="logo" className="w-36"/></li>
+              <li className="p-3 bg-black text-pink-100 font-medium text-xl rounded-2xl flex items-center"><img src={git} alt="logo" className="w-40"/></li>
+            </ul>  
          </div>
          </Container>
       </section>
-      <section className="bg-purple-300 min-h-[500px]">
+      <section className="min-h-[500px]">
         <Container>
           <div className="flex flex-col items-center p-12 gap-4">
-            <h1 className="text-5xl shinning-2 div1 mb-4">Projetos</h1>
-            <ul className="flex flex-wrap gap-1">
+            <h1 className="text-5xl text-pink-50 shinning-2 div1 mb-4">Projetos</h1>
+            <ul className="flex flex-wrap justify-center gap-1">
               {repos
               .filter(e => (repositorios.includes(e.name)))
               .map((repo, i) => (
