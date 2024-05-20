2ca81332252a9f03f485a56f48729439f5ae0504
Mariana Antonia <marievaristo889@gmail.com>
2024-05-20 10:52:12 -0300

    refatoração

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -48,7 +48,7 @@ function Main() {
               <p
                 className="text-4xl font-semibold div1 shinning-3 font-poppins"
                 style={{ color: theme.color }}>
-                  Desenvolvedora Frontend
+                Desenvolvedora Frontend
               </p>
             </div>
             <img src={mari1} alt="foto mariana" className="w-[38%] rounded-full border-4 border-pink-400 float-animation" />
@@ -58,44 +58,45 @@ function Main() {
 
       <SobreMim />
 
-      <section 
-      className="min-h-[500px] pb-9" 
-      id="projeto" 
-      onClick={handleClick}
-      style={{background: theme.bgProjetos}}>
-          <div
-            className="flex flex-col items-center p-12 gap-4 w-full overflow-hidden"
-            ref={carossel}
-          >
-            <h1 
-            className="text-5xl shinning-2 div1 mb-5 font-poppins" 
+      <section
+        className="pb-9"
+        id="projeto"
+        onClick={handleClick}
+        style={{ background: theme.bgProjetos }}
+      >
+        <div
+          className="flex flex-col items-center p-12 gap-4 w-full overflow-hidden"
+          ref={carossel}
+        >
+          <h1
+            className="max-sm:text-3xl text-5xl 
+            shinning-2 div1 mb-5 font-poppins"
             id="projetos"
-            style={{color: theme.color}}
-            >
-              Projetos
-              </h1>
-            <motion.ul
-              className="flex items-center p-2 cursor-grab"
-              whileTap={{ cursor: "grabbing" }}
-              drag="x"
-              dragConstraints={{ left: -Width, right: Width }}
-            >
-              {carregando ?
-                <div className="text-pink-500 text-2xl font-bold border-2 p-2 font-poppins">Carregando....</div>
-                : repositorios
-                  .filter(e => (projetos.map(element => element.nome).includes(e.name)))
-                  .map((repo, i) => (
-                    <Projetos
-                      key={i}
-                      name={repo.name}
-                      homepage={repo.homepage}
-                      deploy={repo.html_url}
-                      linguagem={repo.language}
-                    />
-                  ))
-              }
-            </motion.ul>
-          </div>
+            style={{ color: theme.color }}>
+             Projetos
+          </h1>
+          <motion.ul
+            className="flex items-center p-2 cursor-grab"
+            whileTap={{ cursor: "grabbing" }}
+            drag="x"
+            dragConstraints={{ left: -Width, right: Width }}
+          >
+            {carregando ?
+              <div className="text-pink-500 max-sm:text-lg text-2xl font-bold border-2 p-2 font-poppins">Carregando....</div>
+              : repositorios
+                .filter(e => (projetos.map(element => element.nome).includes(e.name)))
+                .map((repo, i) => (
+                  <Projetos
+                    key={i}
+                    name={repo.name}
+                    homepage={repo.homepage}
+                    deploy={repo.html_url}
+                    linguagem={repo.language}
+                  />
+                ))
+            }
+          </motion.ul>
+        </div>
       </section>
     </main>
   );
--- src/components/main/projetos/index.jsx
+++ src/components/main/projetos/index.jsx
@@ -1,8 +1,8 @@
-import javascript  from '../../../assets/images/js.png'
-import html  from '../../../assets/images/html-5.png'
+import javascript from '../../../assets/images/js.png'
+import html from '../../../assets/images/html-5.png'
 import css from '../../../assets/images/css-3.png'
 
-import PropTypes  from "prop-types"
+import PropTypes from "prop-types"
 
 function Projetos({ name, homepage, deploy, linguagem }) {
    const imgs = {
@@ -15,25 +15,39 @@ function Projetos({ name, homepage, deploy, linguagem }) {
 
    return (
       <li
-         className="w-[500px] min-h-[400px] rounded duration-300 ease-in-out hover:scale-105 list-none flex flex-col items-center justify-center"
+         className="max-sm:w-96 w-[500px] min-h-[300px] 
+         rounded duration-300 ease-in-out hover:scale-105 list-none flex flex-col items-center justify-center"
       >
          <div className="p-5 flex flex-col items-center justify-between gap-3">
             <iframe
                src={homepage}
                title={name}
-               className="bg-white w-[400px] h-[300px] rounded-lg border-2 border-pink-500"
+               className="max-sm:w-80 w-[400px] max-sm:h-56 h-[300px] bg-white rounded-lg border-2 border-pink-500"
                style={{ backgroundSize: "cover" }}
                scrolling="no"
                sandbox="allow-scripts allow-same-origin allow-presentation"
             />
-            <h1 className="text-lg text-pink-500 font-semibold pb-3 font-poppins">{name}</h1>
+            <h1 className="max-sm:text-base text-lg
+             text-pink-500 font-semibold pb-3 font-poppins">
+               {name}
+            </h1>
             <hr className="border border-pink-500 w-full" />
             <ul className="flex items-center justify-between w-full mt-4">
                <li className="list-none">
-                  <img src={imgLiguagem} alt={linguagem} className="w-11 rounded-lg" />
+                  <img
+                     src={imgLiguagem}
+                     alt={linguagem}
+                     className="max-sm:w-7 w-11 max-sm:rounded-none rounded-lg"
+                  />
                </li>
                <li className="list-none">
-                  <a href={homepage ? homepage : deploy} target="_blank" rel="noopener noreferrer" className="text-pink-50 p-2 bg-pink-500 rounded font-poppins">Demo</a>
+                  <a
+                     href={homepage ? homepage : deploy}
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="max-sm:text-xs text-base text-pink-50 p-2 bg-pink-500 rounded font-poppins">
+                     Demo
+                  </a>
                </li>
             </ul>
          </div>
