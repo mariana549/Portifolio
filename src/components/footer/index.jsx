3ebd6b3d1fb4107d0f42828726544e1acc8ba07e
Mariana Antonia <marievaristo889@gmail.com>
2024-05-20 10:32:54 -0300

    refactor: :recycle: refatorando

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -10,33 +10,61 @@ import mari2 from "../../assets/images/mari2.jpg"
 import { handleClick } from "../pegarId";
 
 function Footer() {
-   
+
    const handleSubir = () => {
-      window.scrollTo(0,0)
+      window.scrollTo(0, 0)
    }
 
    return (
       <footer className="bg-black">
          <Container>
-         <aside className="py-12" id="contato" onClick={handleClick}>
-            <h1 className="text-pink-500 text-4xl font-bold pb-9 text-center font-poppins">Contato</h1>
-            <div className="flex items-center justify-between">
-              <aside className="flex flex-col items-start">
-                <p className="text-pink-500 text-2xl font-medium pointer-events-none font-poppins">Entre em contato comigo por: </p>
-                <ul className="py-4 flex gap-5">
-                  <li className="flex items-center gap-2 hover:border p-2 rounded">
-                    <img src={In} alt="" className="w-5 h-5" />
-                    <a href="http://www.linkedin.com/in/mariana-a-6694b5245/" target="_blank" rel="noopener noreferrer" className="text-pink-100 text-base font-bold font-poppins">Linkedin</a>
-                  </li>
-                  <li className="flex items-center gap-2 hover:border p-2 rounded">
-                    <img src={whats} alt="" className="w-5 h-5" />
-                    <a href="https://contate.me/marianaantonia" target="_blank" rel="noopener noreferrer" className="text-pink-100 text-base font-bold font-poppins">WhatsApp</a>
-                  </li>
-                </ul>
-              </aside>
-              <img src={mari2} alt="mariana imagem2" className="w-64 rounded-3xl border-4 border-pink-500" />
-            </div>
-          </aside>
+            <aside className="py-12" id="contato" onClick={handleClick}>
+               <h1 className="max-sm:text-3xl text-4xl 
+            text-pink-500 font-bold pb-9 text-center font-poppins">
+                  Contato
+               </h1>
+               <div className="max-sm:flex-col 
+                              flex items-center justify-between gap-2">
+                  <aside className="max-sm:items-center items-start
+                                    flex flex-col">
+                     <p className="max-sm:text-lg text-2xl
+                                 text-pink-500 font-medium pointer-events-none font-poppins">
+                        Entre em contato comigo por:
+                     </p>
+                     <ul className="py-4 flex gap-5">
+                        <li className="flex items-center gap-2 hover:border p-2 rounded">
+                           <img
+                              src={In}
+                              alt="logo linkedin"
+                              className="max-sm:w-3 max-sm:h-3 w-5 h-5" />
+                           <a
+                              href="http://www.linkedin.com/in/mariana-a-6694b5245/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="max-sm:text-[10px] text-base
+                               text-pink-100 font-bold font-poppins">
+                              Linkedin
+                           </a>
+                        </li>
+                        <li className="flex items-center gap-2 hover:border p-2 rounded">
+                           <img
+                              src={whats}
+                              alt="logo WhatsApp"
+                              className="max-sm:w-3 max-sm:h-3 w-5 h-5" />
+                           <a
+                              href="https://contate.me/marianaantonia"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="max-sm:text-[10px] text-base
+                               text-pink-100 font-bold font-poppins">
+                              WhatsApp
+                           </a>
+                        </li>
+                     </ul>
+                  </aside>
+                  <img src={mari2} alt="mariana imagem2" className="w-64 rounded-3xl border-4 border-pink-500" />
+               </div>
+            </aside>
             <div className="flex w-full justify-between items-center 
             max-sm:flex-col">
                <ul>
